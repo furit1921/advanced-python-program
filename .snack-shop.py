df9ad02caa849c69d2cfc825b82f54ce7d856408
furit1921 <shaurya.jampana2015@gmail.com>
2026-08-24 19:10:39 +0200

    datatypes

--- .snack-shop.py
+++ .snack-shop.py
@@ -20,3 +20,34 @@ sales_price = price - 0.25
 print("sales price:", sales_price)
 double_stock = quantity * 2
 print("double stock:", double_stock)
+
+price = 9.99
+quantity = 10
+
+print("is the price under 10?",price < 10 )
+print("is the quantity over 6?", quantity > 6)
+print("is the price exactly 9.99?", price == 9.99)
+
+snack_item = "lays barbecue chips"
+shop_name = "YUMMY" + " " + "DETERMINATION"
+
+print(snack_item)
+print(shop_name)
+
+print("length of shop name:", len(shop_name))
+print("length of snack item:", len(snack_item))
+print("first letter of shop name:"+ shop_name[0])
+print("last letter of shop name:" + shop_name[-1])
+print("first letter of snack item:" + snack_item[0])
+print("last letter of snack item:" + snack_item[-1])
+
+
+
+price_a = 9.99
+price_b = 7.99
+
+temp = price_a
+price_a = price_b
+price_b = temp
+
+print("after swap", price_a , "and", price_b)
