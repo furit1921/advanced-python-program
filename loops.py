386b77fd5ace97647a98cc275a6d616840b0b868
furit1921 <shaurya.jampana2015@gmail.com>
2026-08-31 19:11:34 +0200

    datatypes

---
+++ loops.py
@@ -0,0 +1,18 @@
+str = "TYRAPOBIA"
+for char in str:
+    print(char)
+for i in range(0,11,2):
+    print(i)
+for i in range(1,11):
+    print(i)
+for i in range(11):
+    print(i)
+for i in range(10,0,-1):
+    print(i)
+
+adj = ["deadly", "bizarre", "scary", "terrifying"]
+reason = ["spiders", "murderous clowns", "heights", "ghosts"]
+
+for x in adj:
+    for y in reason:
+        print(x,y)
